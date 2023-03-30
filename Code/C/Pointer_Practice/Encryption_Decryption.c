1c84065d05b6a988d1294b9842d8234df720055d
Pratyush Niraula <pratyushniraula21@gmail.com>
2023-03-30 16:43:25 -0500

    committtt

--- Code/C/Pointer_Practice/Encryption_Decryption.c
+++ Code/C/Pointer_Practice/Encryption_Decryption.c
@@ -1,14 +1,23 @@
 #include "stdlib.h"
 #include "string.h"
+#include "stdio.h"
 
 int main(int argc, char const *argv[])
 {
     /* code */
+    puts("Hello World bro");
+    scanf("%d", &argc);
+    printf("%d", argc);
     //create a file with encryption and decryption
+
     //file will include the key 
+    
     //another file will include the message you want to encrypt
+    
     //encrypted message will be stored in another file and decrypted will be outputted in the terminal
+    
     //encryption and decryption will be done using XOR operation
+    
     //unordered_map<char, char> key; will store the key
 
     return 0;
